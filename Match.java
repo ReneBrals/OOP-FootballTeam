c5bbe4e8f1feb2cb39a39c1967c55d96058a4920
EmilioOldenziel <emilio.oldenziel@gmail.com>
2014-06-08 18:36:04 +0200

    Match.java added

---
+++ Match.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Match {
+	
+      private Int Home;
+      private Int Away;
+      private String Opponent;
+	  
+	    public int getHome() {
+	        return Home;
+	    }
+	  
+	    public void setHome(int home) {
+	        Home = home;
+	    }
+	    
+	    public int getAway() {
+	        return Away;
+	    }
+	  
+	    public void setAway(int away) {
+	        Away = away;
+	    }
+	    
+	    public String getOpponent() {
+	        return Opponent;
+	    }
+	  
+	    public void setOpponent(String opponent) {
+	        Opponent = opponent;
+	    }
+	  
+}
